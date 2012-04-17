7f4b43382eb0e62cc38bb0a44e892b111cb403cc
Your Name <brian.brianschweitzer@gmail.com>
2012-04-16 22:23:25 -0400

    Update AMG to new search URLs; add classical track search.

--- SearchAMG.py
+++ SearchAMG.py
@@ -3,22 +3,20 @@
 PLUGIN_NAME = u"Search AMG"
 PLUGIN_AUTHOR = u"Brian Schweitzer"
 PLUGIN_DESCRIPTION = "Search AMG"
-PLUGIN_VERSION = "0.2"
+PLUGIN_VERSION = "0.3"
 PLUGIN_API_VERSIONS = ["0.9.0", "0.10","0.11","0.12","0.13","0.14","0.15","0.16"]
 
-from PyQt4 import QtCore
+from PyQt4 import QtCore, QtGui
 from picard.cluster import Cluster
 from picard.util import webbrowser2
-from picard.ui.itemviews import BaseAction, register_cluster_action
-from picard.ui.itemviews import BaseAction, register_album_action
-from picard.ui.itemviews import BaseAction, register_file_action
+from picard.ui.itemviews import BaseAction, register_cluster_action, register_album_action, register_file_action, BaseTreeView
 from picard.metadata import register_track_metadata_processor
 
 class SearchAMGR(BaseAction):
     NAME = "Search AMG for Release"
     def callback(self, objs):
         cluster = objs[0]
-        url = "http://wc10.allmusic.com/cg/amg.dll?P=amg&opt1=2&sql="
+        url = "http://www.allmusic.com/search/album/"
         url += QtCore.QUrl.toPercentEncoding(cluster.metadata["album"])
         webbrowser2.open(url)
 register_cluster_action(SearchAMGR())
@@ -28,17 +26,26 @@ class SearchAMGA(BaseAction):
     NAME = "Search AMG for Artist"
     def callback(self, objs):
         cluster = objs[0]
-        url = "http://wc09.allmusic.com/cg/amg.dll?P=amg&opt1=1&sql="
-        url += QtCore.QUrl.toPercentEncoding(cluster.metadata["artist"])
+        url = "http://www.allmusic.com/search/artist/"
+        url += QtCore.QUrl.toPercentEncoding(cluster.metadata["albumartist"])
         webbrowser2.open(url)
 register_cluster_action(SearchAMGA())
 register_album_action(SearchAMGA())
 
-class SearchAMGT(BaseAction):
-    NAME = "Search AMG for Track"
+class SearchAMGTc(BaseAction):
+    NAME = "Search AMG (non-classical music) for Track"
     def callback(self, objs):
         file = objs[0]
-        url = "http://wc10.allmusic.com/cg/amg.dll?P=amg&opt1=3&sql="
+        url = "http://www.allmusic.com/search/song/"
+        url += QtCore.QUrl.toPercentEncoding(file.metadata["title"])
+        webbrowser2.open(url)
+register_file_action(SearchAMGTc())
+
+class SearchAMGT(BaseAction):
+    NAME = "Search AMG (classical music) for Track"
+    def callback(self, objs):
+        file = objs[0]
+        url = "http://www.allmusic.com/search/work/"
         url += QtCore.QUrl.toPercentEncoding(file.metadata["title"])
         webbrowser2.open(url)
 register_file_action(SearchAMGT())
