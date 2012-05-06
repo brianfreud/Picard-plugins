d94bf3c5bb451d92a3e4ff322e65faccbb2b1fd2
Your Name <brian.brianschweitzer@gmail.com>
2012-05-06 00:00:54 -0400

    Bug fixes to Amazon, eBay plugins

--- SearchAmazon3.py
+++ SearchAmazon3.py
@@ -7,7 +7,10 @@ PLUGIN_VERSION = "0.2"
 PLUGIN_API_VERSIONS = ["0.9.0", "0.10","0.11","0.12","0.13","0.14","0.15","0.16"]
 
 from PyQt4 import QtCore
+from picard.album import Album
 from picard.cluster import Cluster
+from picard.file import File
+from picard.track import Track, NonAlbumTrack
 from picard.util import webbrowser2
 from picard.ui.itemviews import BaseAction, register_album_action, register_cluster_action, register_add_plugin_submenu
 
